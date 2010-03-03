5072116351cf0e80ac55c194c3dbee08d22a886b
Ben Hyde <bhyde@d72.cozy.org>
2010-03-03 15:44:58 -0500

    /tmp/diff

--- vows.lisp
+++ vows.lisp
@@ -2,29 +2,57 @@
 
 (in-package "VOWS")
 
+(deftype trigger () 
+  "Each trigger is given a # to denote it so they can be deleted.  While this number
+is only unique with a given vow some effort is made to keep them from overlapping."
+  'fixnum)
+
+(defgeneric create-trigger (t t)
+  (:documentation "Given a vow and a lambda install as a trigger on the vow's fulfilment, and return trigger id.  The caller must be holding the vow's lock."))
+
+(defgeneric delete-trigger (t t)
+  (:documentation "Given a vow and either the trigger id or the lambda delete it from the vow. The caller must be holding the vow's lock."))
+
+(defgeneric keep-vow (t))
+
+(defgeneric enqueue (t t))
+
+(defgeneric fulfil (t))
+
+(defvar *trigger-generator* 10000)
+
 (defclass vow ()
   ((run-state :initform :new :type (member :new :enqueued :keeping :kept :failed ))
    (lock :initform (bt:make-lock))
    (lambda :initarg :lambda)
    (results :initform nil :type list)
    (error? :initform nil :type (or null condition))
-   (waiting :initform nil :type list)))
+   (max-trigger :initform (prog1
+                              *trigger-generator*
+                            (when (< (- most-positive-fixnum 10000) *trigger-generator*)
+                              (setf *trigger-generator* 0))
+                            (incf *trigger-generator* 1000)))
+   (triggers :initform nil :type list)))
 
 (defun fulfiled? (vow)
   (with-slots (run-state) vow
     (or (eq :kept run-state)
         (eq :failed run-state))))
 
-(defmethod add-waiter (vow lock notify)
-  (with-slots (waiting) vow
-    (push (cons lock notify) waiting)))
+(defmethod create-trigger (vow lambda)
+  (with-slots (triggers max-trigger) vow
+    (incf max-trigger)
+    (push (cons max-trigger lambda) triggers)))
 
-(defmethod remove-waiter (vow lock notify)
-  (with-slots (waiting) vow
-    (setf waiting (delete t waiting :test #'(lambda (x y)
-                                              (declare (ignore x))
-                                              (and (eq lock (car y))
-                                                   (eq notify (cdr y))))))))
+(defmethod delete-trigger ((vow vow) (trigger fixnum))
+  (with-slots (triggers) vow
+    (setf triggers (delete trigger triggers :key #'car))))
+
+(defmethod delete-trigger ((vow vow) (trigger function))
+  (with-slots (triggers) vow
+    (setf triggers (delete trigger triggers :key #'cdr))))
+
+
 
 (defmethod print-object ((v vow) stream)
   (with-slots (run-state) v
@@ -44,7 +72,7 @@
       (format stream "~D threads ~D promises" (length threads) (length promises)))))
 
 (defmethod keep-vow ((vow vow))
-  (with-slots (lock lambda results error? run-state waiting) vow
+  (with-slots (lock lambda results error? run-state triggers) vow
     (bt:with-lock-held (lock)
       (setf run-state :keeping)
       (handler-case
@@ -57,8 +85,9 @@
           (setf results :error)
           (setf error? c)
           (setf run-state :failed))))
-    (loop for (other-lock . notify) in waiting
-       do (bt:condition-notify notify))))
+    (loop
+       for trigger? = (pop triggers)
+       while trigger? do (funcall (cdr trigger?) vow))))
 
 (defmethod enqueue ((vow vow) (keeper promise-keeper))
   "Add a single vow to the promises of a promise keeper."
@@ -100,28 +129,42 @@
 (defun vow (lambda)
   (enqueue (make-instance 'vow :lambda lambda) *promise-keeper*))
 
+(defun make-condition-trigger (lock condition)
+  #'(lambda (vow)
+      (declare (ignore vow))
+      (bt:with-lock-held (lock)
+        (bt:condition-notify condition))))
+
 (defmethod fulfil ((vow vow))
   (with-slots (lock run-state results error?) vow
-    (bt:with-lock-held (lock)
-      (labels ((extract-result ()
+    (labels ((extract-result ()
+               (return-from fulfil
                  (case run-state
                    (:kept
                     (values-list results))
                    (:failed
-                    (error error?))
-                   (otherwise
-                    (let ((mylock (bt:make-lock))
-                          (notify (bt:make-condition-variable)))
-                      (add-waiter vow mylock notify)
-                      (bt:condition-wait notify lock))
-                    (extract-result)))))
-        (extract-result)))))
+                    (error error?))))))
+      (bt:with-lock-held (lock)
+        (when (fulfiled? vow)
+          (extract-result)))
+      (let* ((my-lock (bt:make-lock))
+             (my-notify (bt:make-condition-variable))
+             (my-trigger (make-condition-trigger my-lock my-notify)))
+        (bt:with-lock-held (lock)
+          (create-trigger vow my-trigger))
+        (bt:with-lock-held (my-lock)
+          (bt:condition-wait my-notify my-lock))
+        (bt:with-lock-held (lock)
+          (extract-result))))))
+
+
 
 (defun find-fulfiled (vows)
   "Return the first of vows that has been fulfilled."
   (let* ((our-lock (bt:make-lock))
          (got-one (bt:make-condition-variable))
-         (waiting-upon ()))
+         (our-trigger (make-condition-trigger our-lock got-one))
+         (unfulfilled-vows ()))
     (unwind-protect
          (block :finished-vow
            (bt:with-lock-held (our-lock)
@@ -132,16 +175,16 @@
                 (with-slots (lock) v
                   (bt:with-lock-held (lock)
                     (when (fulfiled? v) (return-from :finished-vow v))
-                    (push v waiting-upon)
-                    (add-waiter v our-lock got-one))))
+                    (push v unfulfilled-vows)
+                    (create-trigger v our-trigger))))
              (loop 
                 (loop for v in vows
                    when (fulfiled? v)
                    do (return-from :finished-vow v))
                 (bt:condition-wait got-one our-lock))))
-      (loop for v in waiting-upon
+      (loop for v in unfulfilled-vows
            do (with-slots (lock) v
                 (bt:with-lock-held (lock)
-                  (remove-waiter v our-lock got-one)))))))
+                  (delete-trigger v our-trigger)))))))
 
 
